4cf859b25314d65183e90f0de4a576265f31ce92
piska0921 <negeromu@163.com>
2021-02-10 05:44:12 +1030

    updated category selector style

--- src/components/CategorySelector.js
+++ src/components/CategorySelector.js
@@ -19,20 +19,24 @@ class CategorySelector extends React.Component {
     }
 
     render() {
-        const { categories} = this.props
+        const { categories } = this.props
         const { selectedCategoryId } = this.state
         return (
             <div>
                 <div className="row">
                     {categories.map((category, idx) => {
                         const activeClassName = ( selectedCategoryId === category.id) ? 'category_item col-3 active': 'category_item col-3'
+                        const bColor = ( selectedCategoryId === category.id) ? '#1c2a4f': '#ccc'
+                        const iconColor = ( selectedCategoryId === category.id) ? '#fff': '#555'
                         return (
                             <div className={activeClassName} key={idx} onClick={(event) => this.selectCategory(event, category)}>
                                 <Ionicon 
-                                 className="rounded-circle"
+                                 className="rounded-circle mt-2"
                                  fontSize="50px"
-                                 color="#555"
-                                 icon={category.iconName}/>
+                                 color={iconColor}
+                                 icon={category.iconName}
+                                 style={{ padding: '5px', backgroundColor: bColor}}/>
+                                 <p>{category.name}</p>
                             </div>
                         )
                     })}
--- src/containers/Create.js
+++ src/containers/Create.js
@@ -1,7 +1,45 @@
 import React from 'react'
+import { Tabs, Tab } from '../components/Tabs'
+import CategorySelector from '../components/CategorySelector'
+
+export const categories = [
+    {
+        "id": "1",
+        "name": "travel",
+        "type": "expense",
+        "iconName": "ios-plane"
+    },
+    {
+        "id": "2",
+        "name": "finance",
+        "type": "expense",
+        "iconName": "logo-yen"
+    },
+    {
+        "id": "3",
+        "name": "food",
+        "type": "expense",
+        "iconName": "ios-pizza"
+    },
+    {
+        "id": "4",
+        "name": "cloth",
+        "type": "expense",
+        "iconName": "ios-shirt"
+    },
+
+]
 
 const Create = () => {
-    return <h1></h1>
+    return (
+        <div className="py-3 px-3 mt-4" style={{ backgroundColor: '#fff'}}>
+            <Tabs activeIndex={0} onTabChange={() => { }}>
+                <Tab>Expense</Tab>
+                <Tab>Income</Tab>
+            </Tabs>
+            <CategorySelector categories={categories} onCategorySelected={() => {}}/>
+        </div>)
+
 }
 
 export default Create
